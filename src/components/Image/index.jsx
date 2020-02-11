49b6b9efd4b1c77c00193996a3f7bd0398b280ff
iath-dev <developer.iath@gmail.com>
2020-02-11 08:02:08 -0500

    156. Mostrando las imagenes y enlace para alta resolución

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -8,6 +8,20 @@ const Image = ({ hit }) => {
         <div className="col-12 col-sm-6 cl-md-4 col-lg-3 mb-4">
             <div className="card">
                 <img src={previewURL} alt={tags} className="card-img-top" />
+                <div className="card-body">
+                    <p className="card-text">{likes} Me gustan</p>
+                    <p className="card-text">{views} Views</p>
+                </div>
+                <div className="card-footer">
+                    <a
+                        className="btn btn-primary btn-block"
+                        href={largeImageURL}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        ver imagen
+                    </a>
+                </div>
             </div>
         </div>
      );
